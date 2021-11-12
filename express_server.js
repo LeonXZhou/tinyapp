3ff6c689fde9bb9db03b5b624ee50f294442ff53
LeonXZhou <leon.zhou@queensu.ca>
2021-11-12 02:40:37 +0000

    cleaned up code for submittion

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const PORT = 8080; // default port 8080
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
+    userID: "aJ48lW",
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
@@ -15,12 +15,12 @@ const users = {
   aJ48lW: {
     id: "aJ48lW",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: 'asdf'
   },
   aJ48lq: {
-    id: "aJ48lW",
+    id: "aJ48lq",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: 'asdf'
   }
 };
 
@@ -29,7 +29,7 @@ const users = {
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require('bcryptjs');
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 
 // local js import (helper functions)
 const generateUniqueStringWrapper = require("./URLHelperFunctions");
@@ -51,25 +51,25 @@ app.use(cookieSession({
   keys: ['pi314', 'e278'],
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+}));
+
+// update existing user passwords to be hashed
+users.aJ48lW.password = bcrypt.hashSync("a", 10);
+users.aJ48lq.password = bcrypt.hashSync("a", 10);
 
 
 // GET method handlers
 app.get("/", (req, res) => {
   if (users[req.session.user_id]) {
     res.redirect('urls');
-  }
-  else {
+  } else {
     res.redirect('login');
   }
 });
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
 
 app.get("/urls", (req, res) => {
-  const filteredURL = urlsForUser(req.session.user_id)
+  const filteredURL = urlsForUser(req.session.user_id);
   const templateVars = { urlDatabase: filteredURL, user: users[req.session.user_id], };
   res.render("urls_index", templateVars);
 });
@@ -78,8 +78,7 @@ app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.session.user_id], };
   if (req.session.user_id) {
     res.render("urls_new", templateVars);
-  }
-  else {
+  } else {
     res.render("login", templateVars);
   }
 });
@@ -94,15 +93,21 @@ app.get("/urls/:shortURL", (req, res) => {
       };
       res.render("urls_show", templateVars);
     } else {
-      res.status(400).send('Error 400: you do not have access to this url')
+      res.status(400).send('Error 400: you do not have access to this url either because you'
+      +'are not logged in or this was not a url you created');
     }
+  } else {
+    res.status(404).send('Error 404: short url not found');
   }
-  else { res.status(404).send('Error 404: short url not found') }
 
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL].longURL)
+  if (urlDatabase[req.params.shortURL]) {
+    res.redirect(urlDatabase[req.params.shortURL].longURL);
+  } else {
+    res.status(404).send('Error 404: short url not found');
+  }
 });
 
 app.get("/register", (req, res) => {
@@ -119,13 +124,12 @@ app.get("/login", (req, res) => {
 
 // POST method handlers
 app.post("/urls", (req, res) => {
-  const shortUrl = generateUniqueUrl()
+  const shortUrl = generateUniqueUrl();
   if (req.session.user_id) {
     urlDatabase[shortUrl] = { longURL: req.body.longURL, userID: req.session.user_id };
     res.redirect(`/urls/${shortUrl}`);
-  }
-  else {
-    return res.status(400).send('Error 400: you must login/register first to shorten emails')
+  } else {
+    return res.status(400).send('Error 400: you must login/register first to shorten emails');
   }
 });
 
@@ -135,10 +139,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
       delete urlDatabase[req.params.shortURL];
       res.redirect(`/urls`);
     } else {
-      res.status(400).send('Error 400: you do not have access to this url')
+      res.status(400).send('Error 400: you do not have access to this url');
     }
+  } else {
+    res.status(404).send('Error 404: short url not found');
   }
-  else { res.status(404).send('Error 404: short url not found') }
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
@@ -147,55 +152,63 @@ app.post("/urls/:shortURL/edit", (req, res) => {
       urlDatabase[req.params.shortURL].longURL = req.body.newLongURL;
       res.redirect(`/urls`);
     } else {
-      res.status(400).send('Error 400: you do not have access to this url')
+      res.status(400).send('Error 400: you do not have access to this url');
     }
+  } else {
+    res.status(404).send('Error 404: short url not found');
   }
-  else { res.status(404).send('Error 404: short url not found') }
 });
 
 app.post("/logout", (req, res) => {
-  req.session = null
+  req.session = null;
   res.redirect(`/urls`);
 });
 
 app.post("/register", (req, res) => {
   if (checkEmailUniqueness(req.body.email)) {
-    bcrypt.genSalt(10)
-      .then((salt) => { return bcrypt.hash("req.body.password", salt) })
-      .then((hashedPassword) => {
-        const UID = generateUniqueUserID()
-        users[UID] = {
-          id: UID,
-          email: req.body.email,
-          password: bcrypt.hashSync(req.body.password, 10), //stores hashed password
-        }
-        req.session.user_id = UID;
-        res.redirect(`/urls`);
-      })
-      .catch((err) => { console.log(err) });
-  }
-  else {
-    return res.status(400).send('Error 400: the email you entered is already in use')
+    if (req.body.email && req.body.password) {
+      bcrypt.genSalt(10)
+        .then((salt) => {
+          return bcrypt.hash(req.body.password, salt);
+        })
+        .then((hashedPassword) => {
+          const UID = generateUniqueUserID();
+          users[UID] = {
+            id: UID,
+            email: req.body.email,
+            password: hashedPassword, //stores hashed password
+          };
+          req.session.user_id = UID;
+          res.redirect(`/urls`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      return res.status(400).send('Error 400: password or email cannot be blank');
+    }
+  } else {
+    return res.status(400).send('Error 400: the email you entered is already in use');
   }
 });
 
 app.post("/login", (req, res) => {
   if (!checkEmailUniqueness(req.body.email)) {
-    const serverHashPassword = users[getUID(req.body.email)].password
+    const serverHashPassword = users[getUID(req.body.email)].password;
     bcrypt.compare(req.body.password, serverHashPassword)
       .then((value) => {
         if (value) {
           req.session.user_id = getUID(req.body.email);
           res.redirect(`/urls`);
-        }
-        else {
-          res.status(403).send('Error 403: Login Information Inccorect')
+        } else {
+          res.status(403).send('Error 403: Login Information Inccorect');
         }
       })
-      .catch((err) => { console.log(err) })
-  }
-  else {
-    res.status(403).send('Error 403: No one here with that email! learn to type')
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.status(403).send('Error 403: No one here with that email! learn to type');
   }
 });
 
@@ -203,3 +216,4 @@ app.post("/login", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Express Server Listening on ${PORT}!`);
 });
+
